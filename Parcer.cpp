e9fa55eb59dd32852a9b80cb5aec1db4aec6170d
Kuchma <desferto@mail.ru>
2023-02-21 21:14:33 +0500

    lv

--- Parcer.cpp
+++ Parcer.cpp
@@ -1,18 +1,25 @@
 #include "Parcer.h"
 
-void Parcer::InputOpeningNameFile() 
-{
-	std::fstream file;
-	std::string name;
+void Parcer::InputOpeningNameFile() {
+
+while(!file.is_open()){	
 	std::cout << "Please enter name file : ";
 	getline(std::cin, name);
-	file.open(name, std::ios::in | std::ios::out);
-	std::cin.clear();
-	while (std::cin.get() != '\n');
-	if (file.is_open())
-		std::cout << "File opening!\n";
-	else
-		std::cout << "File opening error\n";
-	file.close();
+	if (!name.rfind(".ini")) {
+		file.open(name, std::ios::in);
+		if (file.is_open()) {
+			std::cout << "\nFile opening!\n\n";
+			break;
+		}
+			else {
+				std::cout << "\nFile opening error, please try again\n\n";
+			}
+	}
+	else{
+		std::cout << "\nThe file entered is not an ini file, please try again;\n\n";
+	}
+	std::cin.clear();			
+}
 
+file.close();
 }
--- Parcer.h
+++ Parcer.h
@@ -3,7 +3,10 @@
 #include "IParcer.h"
 class Parcer : public IParcer
 {
+public:
 	void InputOpeningNameFile() override;
-	
+private:
+	std::ifstream file;
+	std::string name;
 };
 
